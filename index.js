4e60458dc300987f3c963e5f3cdb27455bbcad55
Skorochka <79852838+Skorochka@users.noreply.github.com>
2021-08-16 15:55:08 +0300

    Выполняет дз

---
+++ index.js
@@ -0,0 +1,43 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+// рефы
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('button[data-action="start"]'),
+    btnStop: document.querySelector('button[data-action="stop"]')
+}
+// слушатели событий
+refs.btnStart.addEventListener('click', startColorChange)
+refs.btnStop.addEventListener('click', stopColorChange)
+// хранит id setInterval 
+let idFunctionStart = null;
+
+// функции для кнопок старт и стоп
+function startColorChange() {
+    idFunctionStart = setInterval(selectionColor, 1000),
+    refs.btnStart.setAttribute('disabled', `true`)
+}
+function stopColorChange() {
+    clearInterval(idFunctionStart),
+    refs.btnStart.removeAttribute('disabled')
+
+}
+// функции для генерации цвета 
+
+function selectionColor() {
+    const randomColor = randomIntegerFromInterval(0, 5)
+    bgc = colors[randomColor]
+    refs.body.setAttribute('style', `background-color: ${bgc}`)
+}
+
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
